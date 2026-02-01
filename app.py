70f608cbe8cbd046511f03fcf33de62ede68d88c
jonash-chataut <jonaschataut777@gmail.com>
2026-02-01 16:39:32 +0545

    recomendation system app.py push

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+movies_list=pickle.load(open('movies.pkl','rb'))
+movies_title=movies_list['title'].values
+similarity=pickle.load(open('similarity.pkl','rb'))
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+
+# give the top 5 recommended movies
+def recommend(movie):
+    movie_index=movies_list[movies_list['title']==movie].index[0]
+    
+    distances=similarity[movie_index]
+
+    top_5_closest_movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+
+    recommend_movies=[]
+    recommend_movies_posters=[]
+
+
+    for i in top_5_closest_movies_list:
+
+        movie_id=movies_list.iloc[i[0]].movie_id
+
+        recommend_movies.append(movies_list.iloc[i[0]].title)
+
+        # fetch poster from API of TMDB
+        recommend_movies_posters.append(fetch_poster(movie_id))
+    
+    return recommend_movies,recommend_movies_posters
+
+
+st.title('Movie Recommender System')
+
+selected_movie_name=st.selectbox(
+    'Search here',
+    movies_title   
+)
+
+if st.button('Recommend'):
+    names,posters=recommend(selected_movie_name)
+    
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
